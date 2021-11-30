546edeca4952a45c2ca464e9de55c6b9612aef83
Ademola <ademolaogunmokun@yahoo.com>
2021-11-30 10:57:13 +0100

    Board display

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,8 @@
-p "hello world" 
+require_relative '../lib/draw.rb'
+puts "Starting Tic Tac game"
+players = ['O', "X"]
+current_player = players[rand(2)]
+b = Board.new(current_player)
+b.display()
+puts
+
