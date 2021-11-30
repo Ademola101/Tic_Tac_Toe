01bd3b0cf4e4fc0b1afd09c6d6312fc26f92166e
Ademola <ademolaogunmokun@yahoo.com>
2021-11-30 16:55:50 +0100

    linters

--- bin/main.rb
+++ bin/main.rb
@@ -1,38 +1,37 @@
-require_relative '../lib/draw.rb'
-puts "Starting Tic Tac game"
-puts "Enter player 1 name"
-player_1 = gets.chomp().capitalize
-puts ""
+require_relative '../lib/draw'
+puts 'Starting Tic Tac game'
+puts 'Enter player 1 name'
+player1 = gets.chomp().capitalize
+puts ''
 puts "Enter player 2 name"
-player_2 = gets.chomp().capitalize
+player2 = gets.chomp().capitalize
 puts ""
 
 system 'cls'
 system 'clear'
 
-players = ['O', "X"]
+players = %w[O X]
 current_player = players[rand(2)]
 b = Board.new(current_player)
 puts
-puts " Welcome to the board \n #{player_1} will play as #{players[0]} and #{player_2} will play as #{players[1]}"
+puts " Welcome to the board \n #{player1} will play as #{players[0]} and #{player2} will play as #{players[1]}"
 puts "Let's start"
 
 system 'cls'
 system 'clear'
-
-
-for i in 0..4
+the_loop = 0..4
+the_loop.each do
   b.display()
-  puts "it's #{player_1}'s turn"
+  puts "it's #{player1}'s turn"
   puts "Please select an available cell from the board"
   while true
-    input = gets.chomp().to_i
+    input = gets.chomp()
     case input.to_i
     when input.nil? || !(input.to_i >= 1 && input.to_i < 10)
       next
     when 1..9
-      input
-      puts " #{player_1} value valid"
+      input.to_i
+      puts " #{player1} value valid"
       break
     else puts "invalid value, please select from the available cell only"
     end
@@ -42,7 +41,7 @@ for i in 0..4
   system 'clear'
   
   b.display()
-  puts "it's #{player_2}'s turn"
+  puts "it's #{player2}'s turn"
   puts "Please select an available cell from the board"
   while true
     input = gets.chomp().to_i
@@ -51,7 +50,7 @@ for i in 0..4
       next
     when 1..9
       input
-      puts " #{player_2} value valid"
+      puts " #{player2} value valid"
       break
     else puts "invalid value, please select from the available cell only"
     end
