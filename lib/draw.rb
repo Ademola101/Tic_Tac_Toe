e23e3dbcca6f5be3d02d2e085748e2157a72f17a
Ademola <ademolaogunmokun@yahoo.com>
2021-11-30 17:20:06 +0100

    string literals

--- lib/draw.rb
+++ lib/draw.rb
@@ -1,16 +1,17 @@
 class Board
   BOARD_MAX_INDEX = 2
-  EMPTY_POSITION = ""
+  EMPTY_POSITION = ''
   attr_accessor :current_player
+
   def initialize(current_player)
     @current_player = current_player
     @board = Array.new(BOARD_MAX_INDEX + 1) {Array.new(BOARD_MAX_INDEX + 1) { EMPTY_POSITION } }
   end
 
   def display
-    puts "+- - - - - -+"
+    puts '+- - - - - -+'
     for row in 0..BOARD_MAX_INDEX
-      print "| "
+      print '| '
       for col in 0..BOARD_MAX_INDEX
         s = @board[row][col]
         if s = EMPTY_POSITION
