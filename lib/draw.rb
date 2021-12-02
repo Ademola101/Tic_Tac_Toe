dedc56418a394197bf04e137ae4ae123c21efc9b
shadrach akaade <akaadesm@gmail.com>
2021-12-02 16:36:47 +0100

    comment winner diagonal method

--- lib/draw.rb
+++ lib/draw.rb
@@ -36,7 +36,7 @@ class Board
     true
   end
 
-def winner
+  def winner
   winner = winner_rows()
   if winner
     return winner
@@ -52,4 +52,29 @@ def winner
    # No winners
    return
   end
+
+  # def winner_diagonals
+  #   first_symbol = @board[0][0]
+  #   for index in 1..BOARD_MAX_INDEX
+  #     if first_symbol != @board[index][index]
+  #       break
+  #     elsif index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+  #   return first_symbol
+  #   end
+  #   end
+  #    first_symbol = @board[0][BOARD_MAX_INDEX]
+  #    row_index = 0
+  #    col_index = BOARD_MAX_INDEX
+  #    while row_index < BOARD_MAX_INDEX
+  #    row_index = row_index + 1
+  #    col_index = col_index - 1
+  #    if first_symbol != @board[row_index][col_index]
+  #    break
+  #    elsif row_index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+  #    return first_symbol
+  #    end
+  #    end
+  #    return
+  #    end
+   
 end
