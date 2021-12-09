89db2bb8982454ab65cda6ed88477669d30301a1
YinkTech <layinka4dat@gmail.com>
2021-12-09 09:39:44 +0100

    winner_diagonals

--- lib/draw.rb
+++ lib/draw.rb
@@ -95,6 +95,29 @@ class Board
     end
     return
   end
-
+  
+  def winner_diagonals
+    first_symbol = @board[0][0]
+    for index in 1..BOARD_MAX_INDEX
+      if first_symbol != @board[index][index]
+        break
+      elsif index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+        return first_symbol
+      end
+    end
+    first_symbol = @board[0][BOARD_MAX_INDEX]
+    row_index = 0
+    col_index = BOARD_MAX_INDEX
+    while row_index < BOARD_MAX_INDEX
+      row_index = row_index + 1
+      col_index = col_index - 1
+      if first_symbol != @board[row_index][col_index]
+        break
+      elsif row_index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+        return first_symbol
+      end
+    end
+    return
+  end
   
 end
