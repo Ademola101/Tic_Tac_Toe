5d9056099b4644577fd41b69e678a1d53d362529
YinkTech <layinka4dat@gmail.com>
2021-12-09 09:34:13 +0100

    winner_cols

--- lib/draw.rb
+++ lib/draw.rb
@@ -81,6 +81,20 @@ class Board
     end
     return
   end
+  
+  def winner_cols
+    for col_index in 0..BOARD_MAX_INDEX
+      first_symbol = @board[0][col_index]
+      for row_index in 1..BOARD_MAX_INDEX
+        if first_symbol != @board[row_index][col_index]
+          break
+        elsif row_index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+          return first_symbol
+        end
+      end
+    end
+    return
+  end
 
   
 end
