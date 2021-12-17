86783450c5cd4ea61768b7d3076c776ced07f8a7
shadrach akaade <akaadesm@gmail.com>
2021-12-17 18:31:01 +0100

    remove linter error

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -8,26 +8,22 @@ describe Board do
         expect(board_test.board_full).to eql false if @board == ''
       end
 
-      let(:board_test){ Board.new('X') }
+      let(:board_test) { Board.new('X') }
       it 'should return true if the board is not empty' do
         expect(board_test.board_full).to eql true if @board == 4
       end
     end
   end
 
-  describe "#validate_position" do
+  describe '#validate_position' do
     context 'checking if spots on the board are occupied' do
       it 'should return true if position is not occupied' do
         board_check = Board.new('O')
-        if @board == ''
-          expect(board_check.validate_position(row, col)).to eql true
-        end
+        expect(board_check.validate_position(row, col)).to eql true if @board == ''
       end
       it 'should return not true if position is occupied' do
         board_check = Board.new('O')
-        if @board == '1'
-          expect(board_check.validate_position(row, col)).not_to eql true
-        end
+        expect(board_check.validate_position(row, col)).not_to eql true if @board == '1'
       end
     end
   end
