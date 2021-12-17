87c4223543306a53d5fe21839fd0bb54294531a8
shadrach akaade <akaadesm@gmail.com>
2021-12-17 18:07:43 +0100

    add testing for validate_position method

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -17,4 +17,23 @@ describe Board do
       end
     end
   end
+
+  describe "#validate_position" do
+  context 'checking if spots on the board are occupied' do
+    it 'should return true if position is not occupied' do
+      board_check = Board.new('O')
+      if @board == ''
+          expect(board_check.validate_position(row, col)).to eql true
+      end
+    end
+      it 'should return not true if position is occupied' do
+        board_check = Board.new('O')
+        if @board == '3'
+            expect(board_check.validate_position(row, col)).not_to eql true
+        end
+    end
+    end
+end
+
+
 end
