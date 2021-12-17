1f9b2c76f81542cce666605baae8ad9fd3b16072
Ademola <ademolaogunmokun@yahoo.com>
2021-12-17 17:57:06 +0100

    board full_test final refactoring

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -1,19 +1,16 @@
 require_relative '../lib/logic'
 
 describe Board do
-  describe 'board-full' do
+  describe '#board_full' do
     context 'check if the board is full or not' do
-      it 'should return false if the board is empty'do
-        board_test = Board.new('X')
-        if @board == ''
-          expect(board_test.board_full).to eql false
-        end
+      let(:board_test) { Board.new('X') }
+      it 'should return false if the board is empty' do
+        expect(board_test.board_full).to eql false if @board == ''
       end
-      it "should return true if the board is not empty" do
-        board_test = Board.new('X')
-        if @board == 4
-          expect(board_test.board_full).to eql true
-        end
+
+      let(:board_test){ Board.new('X') }
+      it 'should return true if the board is not empty' do
+        expect(board_test.board_full).to eql true if @board == 4
       end
     end
   end
