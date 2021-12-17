f39d17b41370967de506e47791863908f6cc394b
Ademola <ademolaogunmokun@yahoo.com>
2021-12-17 17:26:03 +0100

    board full_test initial

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -3,13 +3,13 @@ require_relative '../lib/logic'
 describe Board do
   describe 'board-full' do
     context 'check if the board is full or not' do
-      it 'should return false if the board is'do
+      it 'should return false if the board is empty'do
         board_test = Board.new('X')
         if @board == ''
           expect(board_test.board_full).to eql false
         end
       end
-      it "should return true if the board is not true" do
+      it "should return true if the board is not empty" do
         board_test = Board.new('X')
         if @board == 4
           expect(board_test.board_full).to eql true
